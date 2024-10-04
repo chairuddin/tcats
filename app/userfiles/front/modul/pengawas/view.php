e60013f44878b65007158426398b21ea81250aab
root <root@vmi1567766.contaboserver.net>
2024-10-04 17:18:56 +0800

    styling dashboard

--- app/userfiles/front/modul/pengawas/view.php
+++ app/userfiles/front/modul/pengawas/view.php
@@ -1,4 +1,4 @@
-  <main class="wrapper">
+  <main class="wrapper" id="dashboar-page">
     <div class="container">
       <!--
       <form class="search-kompetensi d-flex justify-content-center" action="">
@@ -16,25 +16,26 @@
         </div>
       </form>
 -->
-      <div class="row">
-          <div class="col-12 mb-4">
+      <div class="row justify-content-center">
+          <div class="col-12 col-md-4 mb-4">
+              <div class="d-flex flex-column justify-content-center">
+                  <a href="<?php echo fronturl('scan');?>" class="text-decoration-none text-white d-flex justify-content-center align-items-center">
+                    <div class="border bg-primary rounded text-center w-100 p-4 dashboard-item" >
+                      <i class="fa-solid fa-chart-pie" style="font-size: 32px"></i><br/>
+                      <p class="m-0 fw-medium mt-3">Competency Card Scan [QR Scan]</p>
+                    </div>
+                  </a>
+              </div>
           </div>
-          <div class="d-flex flex-column justify-content-center gap-4" style="height:10vh">
-              <a href="<?php echo fronturl('scan');?>" class="text-decoration-none text-white d-flex justify-content-center align-items-center mx-3">
-                <div class="border d-flex align-items-center gap-3 p-4 w-100 bg-primary rounded" >
-                  <i class="fa-solid fa-chart-pie" style="font-size: 32px"></i>
-                  <p class="m-0 fw-medium">Competency Card Scan [QR Scan]</p>
-                </div>
-              </a>
-          </div>
-
-          <div class="d-flex flex-column justify-content-center gap-4" style="height:">
-              <a href="<?php echo fronturl('index');?>" class="text-decoration-none text-white d-flex justify-content-center align-items-center mx-3">
-                <div class="border d-flex align-items-center gap-3 p-4 w-100 bg-primary rounded" >
-                  <i class="fa-solid fa-chart-pie" style="font-size: 32px"></i>
-                  <p class="m-0 fw-medium">Uji Kompetensi</p>
-                </div>
-              </a>
+          <div class="col-12 col-md-4 mb-4">
+              <div class="d-flex flex-column justify-content-center">
+                  <a href="<?php echo fronturl('index');?>" class="text-decoration-none text-white d-flex justify-content-center align-items-center">
+                    <div class="border bg-primary rounded text-center w-100 p-4 dashboard-item" >
+                      <i class="fa-solid fa-chart-pie" style="font-size: 32px"></i><br/>
+                      <p class="m-0 fw-medium mt-3">Uji Kompetensi</p>
+                    </div>
+                  </a>
+              </div>
           </div>
 
 
