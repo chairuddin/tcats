0ff2c95e41dcd5a6772caf6a04a399da73ba4a78
Romli <roemly@gmail.com>
2024-10-07 11:24:11 +0700

    s

--- app/userfiles/front/template/elearning/footer.php
+++ app/userfiles/front/template/elearning/footer.php
@@ -1,14 +1,9 @@
     <script src="<<<TEMPLATE_URL>>>/asset/js/jquery-3.6.0.min.js"></script>   
     <script src="<<<TEMPLATE_URL>>>/asset/js/script.js"></script>
     <script
-      src="<<<TEMPLATE_URL>>>/asset/js/bootstrap.bundle.min.js"
-      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-      crossorigin="anonymous"
-    ></script>
+      src="<<<TEMPLATE_URL>>>/asset/js/bootstrap.bundle.min.js"></script>
     <script
-      src="<<<TEMPLATE_URL>>>/asset/js/fontawesome.js"
-      crossorigin="anonymous"
-    ></script>
+      src="<<<TEMPLATE_URL>>>/asset/js/fontawesome.js"></script>
     <?php 
 if($_SESSION['msg_toast']!="") {
 	echo $_SESSION['msg_toast'];
